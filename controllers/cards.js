130edc0fa18ac04b4261be2fca7dd37e8a98a7c0
Aleksandr Shirobokov <scenarist95@yandex.ru>
2021-04-18 23:43:20 +1100

    Первое ревью: исправление ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,7 +5,7 @@ const {
   ERROR_400_DELETE_LIKE,
   ERROR_400_PUT_LIKE,
   ERROR_500_DEFAULT,
-  ERROR_404_ID_CARD
+  ERROR_404_ID_CARD,
 } = require('../utils/errors');
 
 module.exports.createCard = (req, res) => {
@@ -14,52 +14,68 @@ module.exports.createCard = (req, res) => {
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-  .then(cards => res.send({data: cards}))
-  .catch(() => res.status(500).send({message:ERROR_500_DEFAULT}))
+  .then((cards) => res.send({ data: cards }))
+  .catch(() => res.status(500).send({ message: ERROR_500_DEFAULT }));
 }
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  Card.create({name, link, owner: req.user._id})
-  .then(card => res.send({data: card}))
+  Card.create({ name, link, owner: req.user._id })
+  .then((card) => res.send({ data: card }))
   .catch((err) => {
-    if(err.name === ERROR_400_CREATE_CARD) {
-      return res.status(400).send({message:ERROR_400_CREATE_CARD})
+    if(err.name === 'ValidationError') {
+      res.status(400).send({ message: ERROR_400_CREATE_CARD });
     }
-      return res.status(500).send({message:ERROR_500_DEFAULT})
+      res.status(500).send({ message: ERROR_500_DEFAULT });
   })
 }
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-  .then(card => res.send({data: card}))
-  .catch(() => res.status(404).send({message:ERROR_404_ID_CARD}))
+  .then((card) => {
+    if(!card === null) {
+      res.send({ data: null })
+    }
+    res.send({ data: card })
+  })
+  .catch((err) => {
+    if(err.name === 'CastError') {
+      res.status(404).send({ message: ERROR_404_ID_CARD });
+    }
+      res.status(500).send({ message: ERROR_500_DEFAULT });
+  });
 }
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId,
-    {$addToSet: {likes: req.user._id}},
-    {new: true},
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
     )
-  .then(card => res.send({data: card}))
+  .then((card) => res.send({ data: card }))
   .catch((err) => {
-    if(err.name === ERROR_400_PUT_LIKE) {
-      return res.status(400).send({message:ERROR_400_PUT_LIKE})
+    if(err.name === 'ValidationError') {
+      res.status(400).send({ message: ERROR_400_PUT_LIKE });
     }
-      return res.status(500).send({message:ERROR_500_DEFAULT})
+    if(err.name === 'CastError') {
+      res.status(404).send({ message: 'Невалидный id' });
+    }
+      res.status(500).send({ message: ERROR_500_DEFAULT });
   })
 }
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId,
-    {$pull: {likes: req.user._id}},
-    {new: true},
+    { $pull: { likes: req.user._id } },
+    { new: true },
     )
-  .then(card => res.send({data: card}))
+  .then((card) => res.send({ data: card }))
   .catch((err) => {
-    if(err.name === ERROR_400_PUT_LIKE) {
-      return res.status(400).send({message:ERROR_400_DELETE_LIKE})
+    if(err.name === 'ValidationError') {
+       res.status(400).send({ message: ERROR_400_DELETE_LIKE });
     }
-      return res.status(500).send({message:ERROR_500_DEFAULT})
+    if(err.name === 'CastError') {
+      res.status(404).send({ message: 'Невалидный id' });
+    }
+      res.status(500).send({ message: ERROR_500_DEFAULT });
   })
 }
--- controllers/users.js
+++ controllers/users.js
@@ -5,59 +5,65 @@ const {
   ERROR_400_UPDATE_USER,
   ERROR_500_DEFAULT,
   ERROR_404_ID_USER,
-  ERROR_400_USER
+  ERROR_400_USER,
 } = require('../utils/errors');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-  .then(users => res.send({data: users}))
-  .catch(() => res.status(500).send({message:ERROR_500_DEFAULT})
-  )
+  .then((users) => res.send({ data: users }))
+  .catch(() => { res.status(500).send({ message: ERROR_500_DEFAULT });
+  })
 }
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
-  .then(user => res.send({data: user}))
+  .then((user) => res.send({ data: user }))
   .catch((err) => {
-    if(err.name === ERROR_404_ID_USER) {
-      return res.status(404).send({message:ERROR_404_ID_USER})
+    if(err.name === 'CastError') {
+    res.status(404).send({ message: ERROR_404_ID_USER });
     }
-      return res.status(500).send({message:ERROR_500_DEFAULT})
+    res.status(500).send({ message: ERROR_500_DEFAULT });
   })
 }
 
 module.exports.createUser = (req, res) => {
-  const {name, about, avatar} = req.body;
-  User.create({name, about, avatar})
-  .then(user => res.send({data: user}))
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+  .then((user) => res.send({ data: user }))
   .catch((err) => {
-    if(err.name === ERROR_400_USER) {
-      return res.status(400).send({message:ERROR_400_USER})
+    if(err.name === 'ValidationError') {
+      res.status(400).send({ message: ERROR_400_USER });
     }
-      return res.status(500).send({message:ERROR_500_DEFAULT})
+      res.status(500).send({ message: ERROR_500_DEFAULT });
   })
 }
 
 module.exports.patchUser = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate({name, about}, req.user._id)
-  .then(user => res.send({data: user}))
+  User.findByIdAndUpdate({ name, about }, req.user._id, { new: true, runValidation: true })
+  .then((user) => res.send({ data: user }))
   .catch((err) => {
-    if(err.name === ERROR_400_UPDATE_USER) {
-      return res.status(400).send({message:ERROR_400_UPDATE_USER})
+    if(err.name === 'ValidationError') {
+      res.status(400).send({ message: ERROR_400_UPDATE_USER });
     }
-      return res.status(500).send({message:ERROR_500_DEFAULT})
+    if(err.name === 'CastError') {
+      res.status(404).send({ message: ERROR_404_ID_USER });
+    }
+      res.status(500).send({ message: ERROR_500_DEFAULT });
   })
 }
 
 module.exports.patchAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate({avatar}, req.user._id,)
-  .then(user => res.send({data: user}))
+  User.findByIdAndUpdate({ avatar }, req.user._id, { new: true, runValidation: true })
+  .then((user) => res.send({ data: user }))
   .catch((err) => {
-    if(err.name === ERROR_400_UPDATE_AVATAR) {
-      return res.status(400).send({message:ERROR_400_UPDATE_AVATAR})
+    if(err.name === 'ValidationError') {
+      res.status(400).send({ message: ERROR_400_UPDATE_AVATAR });
     }
-      return res.status(500).send({message:ERROR_500_DEFAULT})
+    if(err.name === 'CastError') {
+      res.status(404).send({ message: ERROR_404_ID_USER });
+    }
+      res.status(500).send({ message: ERROR_500_DEFAULT });
   })
 }
