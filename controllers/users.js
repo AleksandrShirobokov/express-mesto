8ed9e314d348633b5117c0e61e55b29f1f306b5b
Aleksandr Shirobokov <scenarist95@yandex.ru>
2021-04-22 21:28:11 +1100

    Третье ревью: err.message

--- controllers/users.js
+++ controllers/users.js
@@ -47,7 +47,7 @@ module.exports.patchUser = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidation: true })
     .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.message === 'NotValidId') {
         res.status(404).send({ message: ERROR_404_ID_USER });
       } else if (err.name === 'CastError') {
         res.status(400).send({ message: ERROR_400_CAST_ERROR });
@@ -62,7 +62,7 @@ module.exports.patchAvatar = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidation: true })
     .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.message === 'NotValidId') {
         res.status(404).send({ message: ERROR_404_ID_USER });
       } else if (err.name === 'CastError') {
         res.status(400).send({ message: ERROR_400_CAST_ERROR });
